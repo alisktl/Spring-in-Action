ed765e8b927400fa9110a27d756a3f9c016464e3
alisktl <alisktl@gmail.com>
2022-10-14 07:51:57 +0600

    Chapter 3: JDBC Template

---
+++ chapter03/tacos-jdbctemplate/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,13 @@
+package tacos.data;
+
+import tacos.TacoOrder;
+
+import java.util.Optional;
+
+public interface OrderRepository {
+
+    TacoOrder save(TacoOrder order);
+
+    Optional<TacoOrder> findById(Long id);
+
+}
