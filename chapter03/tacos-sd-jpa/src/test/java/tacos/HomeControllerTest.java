b7b7a4448a331310014b809115e57bae320b2eed
alisktl <alisktl@gmail.com>
2022-10-14 22:59:47 +0600

    Chapter 3: Spring Boot JPA

---
+++ chapter03/tacos-sd-jpa/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tacos.data.IngredientRepository;
+import tacos.data.OrderRepository;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(
+                containsString("Welcome to...")
+            ));
+    }
+}
