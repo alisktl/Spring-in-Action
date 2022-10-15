58a8eaa38e9ceb34b86b85f8224d5ffc0930cc7d
alisktl <alisktl@gmail.com>
2022-10-15 23:25:40 +0600

    Chapter 4: Spring Data Cassandra

---
+++ chapter04/tacos-sd-cassandra/src/main/java/tacos/IngredientUDT.java
@@ -0,0 +1,18 @@
+package tacos;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.UserDefinedType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@UserDefinedType("ingredient")
+public class IngredientUDT {
+
+    private final String name;
+    private final Ingredient.Type type;
+
+}
