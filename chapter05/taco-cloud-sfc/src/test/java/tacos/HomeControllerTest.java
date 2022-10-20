a3d098d06e4fe76a5d3d202bd10fbfdd5d4fa5eb
alisktl <alisktl@gmail.com>
2022-10-20 18:00:33 +0600

    Small changes

--- chapter05/taco-cloud-sfc/src/test/java/tacos/HomeControllerTest.java
+++ chapter05/taco-cloud-sfc/src/test/java/tacos/HomeControllerTest.java
@@ -1,16 +1,15 @@
 package tacos;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import tacos.data.IngredientRepository;
 import tacos.data.OrderRepository;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import tacos.data.TacoRepository;
+import tacos.data.UserRepository;
+import tacos.data.service.OrderAdminService;
 
 @WebMvcTest
 public class HomeControllerTest {
@@ -19,18 +18,20 @@ public class HomeControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
+    private OrderAdminService adminService;
+
+    @MockBean
     private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private TacoRepository designRepository;
 
     @MockBean
     private OrderRepository orderRepository;
 
-    @Test
-    public void testHomePage() throws Exception {
-        mockMvc.perform(get("/"))
-            .andExpect(status().isOk())
-            .andExpect(view().name("home"))
-            .andExpect(content().string(
-                containsString("Welcome to...")
-            ));
-    }
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
 }
--- chapter05/taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ chapter05/taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import tacos.data.IngredientRepository;
 import tacos.data.OrderRepository;
+import tacos.data.TacoRepository;
+import tacos.data.UserRepository;
+import tacos.data.service.OrderAdminService;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,10 +23,22 @@ public class HomeControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
+    private OrderAdminService adminService;
+
+    @MockBean
     private IngredientRepository ingredientRepository;
 
     @MockBean
+    private TacoRepository designRepository;
+
+    @MockBean
     private OrderRepository orderRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
 
     @Test
     public void testHomePage() throws Exception {
