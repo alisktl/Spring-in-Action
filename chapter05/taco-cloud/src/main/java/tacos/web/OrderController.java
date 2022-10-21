59b41b6d722326c671d38a224dfd7e7522bd4557
alisktl <alisktl@gmail.com>
2022-10-21 07:25:26 +0600

    Chapter 5: small changes

--- chapter05/taco-cloud/src/main/java/tacos/web/OrderController.java
+++ chapter05/taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -4,10 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
 import tacos.User;
@@ -28,7 +25,24 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute TacoOrder tacoOrder) {
+        if (tacoOrder.getDeliveryName() == null) {
+            tacoOrder.setDeliveryName(user.getUsername());
+        }
+        if (tacoOrder.getDeliveryStreet() == null) {
+            tacoOrder.setDeliveryStreet(user.getStreet());
+        }
+        if (tacoOrder.getDeliveryCity() == null) {
+            tacoOrder.setDeliveryCity(user.getCity());
+        }
+        if (tacoOrder.getDeliveryState() == null) {
+            tacoOrder.setDeliveryState(user.getState());
+        }
+        if (tacoOrder.getDeliveryZip() == null) {
+            tacoOrder.setDeliveryZip(user.getZip());
+        }
+
         return "orderForm";
     }
 
