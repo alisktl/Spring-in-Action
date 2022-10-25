7fc6563ed150a4fa1f073b4373cfe9b8f73e2ae9
alisktl <alisktl@gmail.com>
2022-10-26 00:04:04 +0600

    Chapter 8

---
+++ chapter08/auth-server/src/test/java/tacos/authorization/AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
